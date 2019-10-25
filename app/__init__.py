e3c9b5c677016b4da4651380cefa5fda8a0cad8e
brayooh <brianndichu.bn@gmail.com>
2019-10-25 17:27:33 +0300

    modify url configurations

--- app/__init__.py
+++ app/__init__.py
@@ -6,19 +6,27 @@ from flask_mail import Mail
 
 from config import config_options
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-bootstrap = Bootstrap(app)
-mail = Mail(app)
+db = SQLAlchemy()
+login_manager = LoginManager()
+bootstrap = Bootstrap()
+mail = Mail()
 login_manager.login_view = 'auth.login'
 login_manager.session_protection = 'strong'
 
 
 def create_app(config_name):
+    app = Flask(__name__)
+
+
+
     app.config.from_object(config_options[config_name])
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)    
     from .auth import auth as auth_blueprint
     from .main import main as main_blueprint
     app.register_blueprint(auth_blueprint)
-    app.register_blueprint(main_blueprint)
+    app.register_blueprint(main_blueprint) 
     return app
