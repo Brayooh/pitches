a8e0e16701f4a4d35d0ab9f9667c19390cf4fb36
brayooh <brianndichu.bn@gmail.com>
2019-10-25 14:56:50 +0300

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-export SECRET_KEY = 'qwertyuiop'
+export SECRET_KEY='qwertyuiop'
 
-export MAIL_USERNAME = 'brianndichu.bn@gmail.com'
+export MAIL_USERNAME='brianndichu.bn@gmail.com'
 
-export MAIL_PASSWORD = 'brayooh'
+export MAIL_PASSWORD='brayooh'
 
 python3.6 manage.py server
